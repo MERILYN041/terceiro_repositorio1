26fd770f86555c2caebc074dd68574aa8f6df3d5
MERILYN041 <144914757+MERILYN041@users.noreply.github.com>
2025-06-02 07:48:03 -0300

    02/06

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    const alternaContrasre = document.getElementById('alterna-contraste');
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -21,9 +21,9 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem';
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
-})
-alternaContrasre.addEventListener('click', function(){
-    document.body.classList.toggle('alto-contraste');
+    alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ })
 })
